506505aa8e5173c7e4c3d78fb8c7196c24e6e443
Shohan-CtRl <66548046+Shohan-CtRl@users.noreply.github.com>
2021-03-17 11:48:08 +0530

    MoodTracker
    Demoed Version of the mood tracker. Working on the Json file approach.

--- frontend/MoodTracker/MoodTracker.js
+++ frontend/MoodTracker/MoodTracker.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
 import "./MoodTracker.css";
 import { Line } from 'react-chartjs-2';
 
@@ -8,66 +8,54 @@ export default class MoodTracker extends Component{
 
         this.state = {
             data: {
-                labels: ["Mon","Tue","Wen","Thu","Sat","Sun"],
-                datasets: [
-                    {
-                        label: "Sad",
-                        backgroundColor: "rgba(255,0,255,0.75)",
-                        data: [4 ,5, 1, 10, 32, 2, 12]
-                    },
-                    {
-                        label: "Happy",
-                        backgroundColor: "rgba(0,255,0,0.75)",
-                        data: [14 ,15, 21, 0, 12, 4, 2]
-                    }
-
+                labels: ["Sun","Mon","Tue","Wen","Thu","Fri","Sat"],
+                datasets: [{
+                    label: "Happy",
+                    backgroundColor: "rgba(255,0,255,0.75)",
+                    data: [4,5,1,10,32,2,12]
+                },
+                {
+                    label: "Sad",
+                    backgroundColor: "rgba(0,255,0,0.75)",
+                    data: [5,6,10,30,32,5,0],
+                }
+                    
                 ]
             }
         }
     }
-    setGradientColor = (canvas, color) => {
+    setGradientColor= (canvas, color) =>{
         const ctx = canvas.getContext('2d');
-        const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+        const gradient = ctx.createLinearGradient(0,0,0,400);
         gradient.addColorStop(0, color);
-        gradient.addColorStop(0.95, "rgba(133,255,144,0.85)");
+        gradient.addColorStop(0.95, "rgba(133,255,144,0.85)")
         return gradient;
     }
-    getChartData = canvas => {
+    getChartData = canvas =>{
         const data = this.state.data;
         if(data.datasets){
-            let colors = ["rgba(255,0,255,0.75)", "rgba(0,255,0,0.75)"];
-            data.datasets.forEach((set, i)=> {
+            let colors = ["rgba(255, 0, 255, 0.75)", "rgb(128,0,128)"];
+            data.datasets.forEach((set,i) =>{
                 set.backgroundColor = this.setGradientColor(canvas, colors[i]);
-                set.borderColor = "white";
+                set.borderColor= "white";
                 set.borderWidth = 2;
-            });
+
+            })
         }
         return data;
     }
     render(){
         return(
-        <div className="moodTracker">
-            <h3>Chart sample</h3>
-            <Line className="moodTracker_graph"
-                options={{
+            <div className="MoodTracker_Container">
+                <h3>Mood Tracker</h3>
+                <Line 
+                options ={{
                     responsive: true
                 }}
                 data={this.getChartData}
-            />
-        </div>
+                />
+            </div>
+            
         )
-        
     }
 }
-
-
-
-// function MoodTracker() {
-    
-//     return (
-// // // //       <div className="dashboard__moodTracker">
-//           <h4>Shuhaib Ahameddf</h4>
-//       </div>
-//     );
-//   }
-// export default MoodTracker;

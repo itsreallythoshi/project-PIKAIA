288124fc14c0b52925f3a67c78aa72a9b1fd1826
Shuhaib Ahamed <shuhaib.2019678@iit.ac.lk>
2021-03-27 13:40:02 +0530

    Revert "Updated ML model"
    This reverts commit 17bf9e8cb7758e2d5b319e1e95d72e67436061cc.

--- backend/flaskBackend_revamped/pikaia/__init__.py
+++ backend/flaskBackend_revamped/pikaia/__init__.py
@@ -9,10 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pikaia.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Deployment Success Check! ML model deployed'
-
 from pikaia.admin import routes
 from pikaia.chatbot import routes
 from pikaia.music_recommender import routes
